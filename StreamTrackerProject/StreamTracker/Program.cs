6a73ae28eade45c6711f98dbf6990c9ef2c76101
Cynthia Pugh <145383145+cwpugh@users.noreply.github.com>
2025-04-06 21:55:52 +0000

    Add program listing functionality

--- StreamTrackerProject/StreamTracker/Program.cs
+++ StreamTrackerProject/StreamTracker/Program.cs
@@ -10,16 +10,16 @@ class Program
         string programName, watchStatus;
         do {
             // show basic menu choices
-            Console.WriteLine("Main Menu");
-            Console.WriteLine("-------------------------------------");
-            Console.WriteLine("To enter a new program, type 'program'");
-            Console.WriteLine("To see a list of programs, type 'list'");
-            Console.WriteLine("If you are finished, type 'quit'");
+            Console.WriteLine("Main Menu: Choose a number below");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("1 - Enter a new program");
+            Console.WriteLine("2 - List current programs");
+            Console.WriteLine("3 - Quit program");
             Console.Write("Enter your choice here: ");
             mainChoice = Console.ReadLine();
 
             // 'program' selection
-            if(mainChoice == "program") {
+            if(mainChoice == "1") {
                 Console.Write("Please enter the program name: ");
                 programName = Console.ReadLine();
                 Console.Write("Have you watched this show? (enter y or n) ");
@@ -29,18 +29,43 @@ class Program
             }
 
             // 'list' selection
-            else if(mainChoice == "list") {
-                Console.WriteLine("You entered 'list'");
-                Console.WriteLine();
+            else if(mainChoice == "2") {
+                 // Specify the path to your text file
+                string fileName = @"movie-list.txt";
+
+                // Check if the file exists
+                if (File.Exists(fileName))
+                {
+                    // Read all lines from the text file
+                    string[] programList = File.ReadAllLines(fileName);
+
+                    // Display each progam
+                    Console.Clear();
+                    Console.WriteLine("Programs");
+                    Console.WriteLine("------------------------");
+                    foreach (string program in programList)
+                    {
+                        Console.WriteLine(program);
+                        
+                        
+                    }
+                    Console.WriteLine("------------------------");
+                    Console.WriteLine("Listing complete. Press any key to continue.");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("No programs exist.");
+                }
             }
 
             // invalid selection
-            else if (mainChoice!="quit") {
+            else if (mainChoice!="3") {
                 Console.WriteLine("'" + mainChoice + "' is an invalid response. Please try again.");
                 Console.WriteLine();
             }
 
-        } while(mainChoice!="quit");
+        } while(mainChoice!="3");
 
         // Message before exiting
         Console.Clear();
